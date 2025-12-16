1660090845cc080982c8ce08fa1b4387cf0e3b89
Jawsk <felipemello490@gmail.com>
2025-12-16 16:57:58 -0300

    Update power-saving-usb.ps1

--- Windows 10/power-saving-usb.ps1
+++ Windows 10/power-saving-usb.ps1
@@ -1,6 +1,12 @@
-Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Enum\USB" -Recurse -ErrorAction SilentlyContinue |
-Where-Object { $_.PSChildName -eq "Device Parameters" } |
-ForEach-Object {
-    New-ItemProperty -Path $_.PsPath -Name "SelectiveSuspendEnabled" -PropertyType DWord -Value 0 -Force | Out-Null
-    New-ItemProperty -Path $_.PsPath -Name "EnhancedPowerManagementEnabled" -PropertyType DWord -Value 0 -Force | Out-Null
+$hubs = Get-CimInstance -ClassName Win32_SerialPort | Select-Object Name, DeviceID, Description
+$powerMgmt = Get-CimInstance -ClassName MSPower_DeviceEnable -Namespace root\wmi
+
+foreach ($p in $powerMgmt) {
+    $IN = $p.InstanceName.ToUpper()
+    foreach ($h in $hubs) {
+        $PNPDI = $h.PNPDeviceID
+        if ($IN -like "*$PNPDI*") {
+            Set-CimInstance -InputObject $p -Property @{Enable = $false}
+        }
+    }
 }
