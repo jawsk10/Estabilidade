8b3cfd9f849227c4bea8be112291b74c9d6be1b7
Jawsk <felipemello490@gmail.com>
2025-12-16 13:54:59 -0300

    atualização

--- Windows 10/tweaks1.bat
+++ Windows 10/tweaks1.bat
@@ -147,7 +147,7 @@ schtasks /Change /tN "Microsoft\Windows\Time Synchronization\ForceSynchronizeTim
 schtasks /Change /tN "Microsoft\Windows\Time Synchronization\SynchronizeTime" /disable
 schtasks /Change /tN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable
 
-::Adapter
+:: Adapter
 for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do REG ADD "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v TcpAckFrequency /t REG_DWORD /d 1 /f
 for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do REG ADD "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v TcpDelAckTicks /t REG_DWORD /d 0 /f
 for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do REG ADD "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v TCPNoDelay /t REG_DWORD /d 1 /f
@@ -687,5 +687,6 @@ REG ADD "HKLM\System\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualiza
 TIMEOUT /t 5
 taskkill /f /im explorer.exe
 start explorer.exe
+
 
 
