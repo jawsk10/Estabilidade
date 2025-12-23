574e0e5dffccce1c60e7e413cfee907dc7332e9e
Jawsk <felipemello490@gmail.com>
2025-12-23 17:16:49 -0300

    Update tweaks.bat

--- Windows 10/tweaks.bat
+++ Windows 10/tweaks.bat
@@ -520,12 +520,10 @@ REG ADD "HKLM\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_SZ /d 
 :: Bcdedit
 bcdedit /set useplatformtick yes
 bcdedit /setdisabledynamictick yes
-bcdedit /set tscsyncpolicy enhanced
 bcdedit /deletevalue useplatformclock
 
 :: Spectre & Meltdown
-REG ADD "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f
-REG ADD "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "1" /f
+REG ADD "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "0" /f
 REG ADD "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f
 REG ADD "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f
 
@@ -834,3 +832,4 @@ taskkill /f /im dwm.exe
 start dwm.exe
 start explorer.exe
 pause
+
